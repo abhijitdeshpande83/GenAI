73bfb218eab2c18312d312b5597799258ccfa40f
Abhijit Deshpande <abhijitdeshpande83@gmail.com>
2025-06-29 23:08:21 -0500

    Dockerized application

--- SupportIQ/src/inference.py
+++ SupportIQ/src/inference.py
@@ -20,25 +20,27 @@ def model_fn(model_dir):
     # base_model = AutoModelForSeq2SeqLM.from_pretrained(peft_config.base_model_name_or_path)
     # model = PeftModel.from_pretrained(base_model, checkpoint_path)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_dir)
-    tokenizer = AutoTokenizer.from_pretrained(model_dir)
+    device = torch.device("cude" if torch.cuda.is_available() else "cpu")
     model.eval()
 
-    return {'model': model,'tokenizer': tokenizer}
+    return {'model': model,'tokenizer': tokenizer, 'device':device}
 
 def predict_fn(data, model_obj):
-    input_text = data['input']
+    input_text = data.get('input', None)
+    if input_text is None:
+        return ValueError("Inpute text is required under key 'input'")
     tokenizer = model_obj['tokenizer']
     model = model_obj['model']
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
-
-    # input_text = [item['input'] for item in data]
+    device = model_obj['device']
 
     inputs = tokenizer(input_text, return_tensors='pt',padding=True, truncation=True).to(device)
     with torch.no_grad():
         outputs =model.generate(**inputs, max_new_tokens=20)
     
     decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    # decoded = [tokenizer.decode(output, skip_special_tokens=True) for output in outputs]
+    
     return {'generated_text': decoded}
+
+def predict(data, model_dir):
+    model_obj = model_fn(model_dir)
+    return predict_fn(data, model_obj)
