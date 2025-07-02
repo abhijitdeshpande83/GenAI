bb6eec168cea2c552ec7c6608cd207b3115be3ee
Abhijit Deshpande <abhijitdeshpande83@gmail.com>
2025-07-02 17:31:18 -0500

    Removed Django api (ECS Faregt) and moved to AWS Lambda for cost saving

--- SupportIQ/supportiq_intent_api/intent_classifier/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IntentClassifierConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'intent_classifier'
--- SupportIQ/supportiq_intent_api/intent_classifier/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, intent_classify, health
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('intent_classify/', intent_classify, name='intent_classify'),
-    path('health', health, name='health')
-]
