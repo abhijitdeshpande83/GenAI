d759b504bec4caccdbc0fd298378c1d1e14c60f8
Abhijit Deshpande <abhijitdeshpande83@gmail.com>
2026-03-14 18:31:19 -0500

    feat: udpate docker file to execute lambda_function

--- TaskPilot/build.sh
+++ TaskPilot/build.sh
@@ -8,20 +8,15 @@ ECR_URI="720332985926.dkr.ecr.us-east-1.amazonaws.com"
 DOCKER_USER=${1}
 IMG_TAG=${2}
 
-#Build Docker Image
-docker buildx build --platform linux/arm64 -t $DOCKER_USER/task-pilot:$IMG_TAG --push .
-
-#Pull image locally
-docker pull $DOCKER_USER/task-pilot:$IMG_TAG 
-
-#Tag image for ECR
-docker tag $DOCKER_USER/task-pilot:$IMG_TAG $ECR_URI/task-pilot:$IMG_TAG
+#Docker login
+docker login
 
 #Login to AWS
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_URI
 
-#Push image to ECR
-docker push $ECR_URI/task-pilot:$IMG_TAG
+#Build and push to both registries
+docker buildx build --platform linux/amd64 --provenance=false \
+        -t $DOCKER_USER/task-pilot:$IMG_TAG -t $ECR_URI/task-pilot:$IMG_TAG --push .
 
 #Dispaly message
 echo "------ Image pushed to ECR ------"
