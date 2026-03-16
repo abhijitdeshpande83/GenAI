990f94475c6b040e26516d767e542fcb14027f38
Abhijit Deshpande <abhijitdeshpande83@gmail.com>
2026-03-16 13:21:13 -0500

    fix: resolve requirement file conflit

--- TaskPilot/src/lambda_function.py
+++ TaskPilot/src/lambda_function.py
@@ -3,26 +3,13 @@ import json
 
 def lambda_function(event, context):
 
-        # Handle preflight OPTIONS
-    if event.get("httpMethod") == "OPTIONS":
-        return {
-            "statusCode": 204,
-            "headers": {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "*",
-                "Access-Control-Allow-Methods": "POST, OPTIONS",
-            },
-            "body": ""
-        }
-
-
     try:
         body=json.loads(event['body']) if 'body' in event else event
         usr_input = body.get('usr_input',None)
 
         if not usr_input:
             raise ValueError("Input text is missing")
-        
+    
         response = run_agent(usr_input)
     
         return {
@@ -30,9 +17,6 @@ def lambda_function(event, context):
             "body": json.dumps(response),
             "headers":
             {    
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "*",
-                "Access-Control-Allow-Methods": "*",
                 "Content-Type":"application/json"
             }
         }
@@ -43,9 +27,6 @@ def lambda_function(event, context):
             "body": json.dumps({"error":str(e)}),
             "headers":
             {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "*",
-                "Access-Control-Allow-Methods": "*",
                 "Content-Type": "application/json"
             }
         }
