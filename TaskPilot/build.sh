5f28b10eb5baba64e3e05894160c736f48540e2d
Abhijit Deshpande <abhijitdeshpande83@gmail.com>
2026-03-22 15:46:19 -0500

    fix: merge conflit issue resolving

--- TaskPilot/build.sh
+++ TaskPilot/build.sh
@@ -10,6 +10,9 @@ IMG_TAG=${2}
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e979f2f (feat: add docker file)
 #Docker login
 docker login
 =======
@@ -22,16 +25,22 @@ docker pull $DOCKER_USER/task-pilot:$IMG_TAG
 #Tag image for ECR
 docker tag $DOCKER_USER/task-pilot:$IMG_TAG $ECR_URI/task-pilot:$IMG_TAG
 >>>>>>> 0648ecc (feat: add docker file)
+<<<<<<< HEAD
 =======
 #Docker login
 docker login
 >>>>>>> d759b50 (feat: udpate docker file to execute lambda_function)
+=======
+>>>>>>> e979f2f (feat: add docker file)
 
 #Login to AWS
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_URI
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e979f2f (feat: add docker file)
 #Build and push to both registries
 docker buildx build --platform linux/amd64 --provenance=false \
         -t $DOCKER_USER/task-pilot:$IMG_TAG -t $ECR_URI/task-pilot:$IMG_TAG --push .
@@ -39,11 +48,14 @@ docker buildx build --platform linux/amd64 --provenance=false \
 #Push image to ECR
 docker push $ECR_URI/task-pilot:$IMG_TAG
 >>>>>>> 0648ecc (feat: add docker file)
+<<<<<<< HEAD
 =======
 #Build and push to both registries
 docker buildx build --platform linux/amd64 --provenance=false \
         -t $DOCKER_USER/task-pilot:$IMG_TAG -t $ECR_URI/task-pilot:$IMG_TAG --push .
 >>>>>>> d759b50 (feat: udpate docker file to execute lambda_function)
+=======
+>>>>>>> e979f2f (feat: add docker file)
 
 #Dispaly message
 echo "------ Image pushed to ECR ------"
