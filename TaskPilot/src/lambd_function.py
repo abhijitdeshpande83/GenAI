d1c897abc2f1591297114dc760865c0821c718e0
Abhijit Deshpande <abhijitdeshpande83@gmail.com>
2026-03-22 15:46:19 -0500

    fix: merge conflit issue resolving

---
+++ TaskPilot/src/lambd_function.py
@@ -0,0 +1,32 @@
+from src.runner import run_agent
+import json
+
+def lambda_function(event, context):
+
+    try:
+        body=json.load(event['body']) if 'body' in event else event
+        usr_input = body.get('usr_input',None)
+
+        if not usr_input:
+            raise ValueError("Input text is missing")
+        
+        result = run_agent(usr_input)
+    
+        return {
+            "statusCode":200,
+            "body": json.dumps(result),
+            "headers":
+            {
+                "Content-Type":"application/json"
+            }
+        }
+
+    except Exception as e:
+        return {
+            "statusCode":500,
+            "body": json.dumps({"error":str(e)}),
+            "headers":
+            {
+                "Content-Type":"application/json"
+            }
+        }
